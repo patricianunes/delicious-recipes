3ad7fe64a203a42a85c872c2e8a5109c79737235
Patricia Campos <patricia@krint.io>
2019-06-09 19:50:44 +1000

    Search recipe app

---
+++ src/components/DeliciousForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DeliciousForm = props => (
+  <form onSubmit={props.getRecipe} style={{ marginBottom: "2rem" }}>
+    <input className="form__input" type="text" name="recipeName" />
+    <button className="form__button">Search</button>
+  </form>
+);
+
+export default DeliciousForm;
