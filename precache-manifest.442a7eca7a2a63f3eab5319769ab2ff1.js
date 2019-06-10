50846c0edd9a56cc3b1c9d10a683036bc4292c86
Patricia Campos <patricia@krint.io>
2019-06-10 10:09:12 +1000

    Updates

---
+++ precache-manifest.442a7eca7a2a63f3eab5319769ab2ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed996db39516f40dd1649f191d62add",
+    "url": "/delicious-recipes/index.html"
+  },
+  {
+    "revision": "d64445c5c906892219b3",
+    "url": "/delicious-recipes/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6208bb1c231c54ce6270",
+    "url": "/delicious-recipes/static/css/main.9b3b67fd.chunk.css"
+  },
+  {
+    "revision": "d64445c5c906892219b3",
+    "url": "/delicious-recipes/static/js/2.6c4bcd28.chunk.js"
+  },
+  {
+    "revision": "6208bb1c231c54ce6270",
+    "url": "/delicious-recipes/static/js/main.7c3f9255.chunk.js"
+  },
+  {
+    "revision": "1bbbc7614094be5a1325",
+    "url": "/delicious-recipes/static/js/runtime~main.60fcd059.js"
+  }
+]);
