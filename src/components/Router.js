692e34098cba3b96b7e2f3e7998fe9c068835041
Patricia Campos <patricia@krint.io>
2019-06-10 11:39:05 +1000

    change browserRouter for hashRouter to make ghpages work

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App";
 import Recipe from "./Recipe";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/recipe/:id" component={Recipe} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
